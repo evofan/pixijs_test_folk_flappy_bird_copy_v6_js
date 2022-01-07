914b85a97692a2ab558564d28910bf4bb0381604
evo fan <evofan@outlook.jp>
2022-01-07 09:51:21 +0900

    Undo index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ import { displayParamText } from "./helper/text.js";
 import { displaySprite } from "./helper/sprite.js";
 import { Common } from "./helper/common.js";
 
-import { Bird } from "./Bird.js";
-import { Tube } from "./Tube.js";
+// import { Bird } from "./Bird.js";
+// import { Tube } from "./Tube.js";
 
-import img_snowman from "./images/pic_yukidaruma.png";
-import img_bg from "./images/pic_darksky_bg.jpg";
+// import img_snowman from "./images/pic_yukidaruma.png";
+// import img_bg from "./images/pic_darksky_bg.jpg";
 
 import img_frame1 from "./images/frame-1.png";
 import img_frame2 from "./images/frame-2.png";
 import img_frame3 from "./images/frame-3.png";
 import img_frame4 from "./images/frame-4.png";
-
 
 import "./main.css";
 
@@ -35,6 +34,7 @@ const TUBE_POS_LIST = [
     CANVAS_WIDTH_HEIGHT + 250,
     CANVAS_WIDTH_HEIGHT + 480,
 ];
+
 /*
 const renderer = PIXI.autoDetectRenderer(canvasWidthHeight, canvasWidthHeight, {
     backgroundColor: 0xc1c2c4,
@@ -171,3 +171,178 @@ const update = (time) => {
     console.log("update()")
 
 }
+
+
+
+/*
+class Bird {
+    speedY = 0;
+    sprite = new PIXI.Sprite();
+    isDied = false;
+
+    textureCounter = 0;
+    updateTexture = () => {
+        if (this.isDied) return;
+        this.sprite.texture =
+            PIXI.loader.resources[BIRD_FRAME_LIST[this.textureCounter++]].texture;
+
+        if (this.textureCounter === BIRD_FRAME_LIST.length) this.textureCounter = 0;
+    };
+
+    updateSprite = () => {
+        this.speedY += GRAVITY / 70;
+        this.sprite.y += this.speedY;
+        this.sprite.rotation = Math.atan(this.speedY / GAME_SPEED_X);
+
+        let isCollide = false;
+        const { x, y, width, height } = this.sprite;
+        this.tubeList.forEach((d) => {
+            if (d.checkCollision(x - width / 2, y - height / 2, width, height))
+                isCollide = true;
+        });
+        if (y < -height / 2 || y > canvasWidthHeight + height / 2) isCollide = true;
+
+        if (isCollide) {
+            this.onCollision();
+            this.isDied = true;
+        }
+    };
+
+    addSpeed(speedInc) {
+        this.speedY += speedInc;
+        this.speedY = Math.max(-GRAVITY, this.speedY);
+    }
+
+    reset() {
+        this.sprite.x = canvasWidthHeight / 6;
+        this.sprite.y = canvasWidthHeight / 2.5;
+        this.speedY = 0;
+        this.isDied = false;
+    }
+
+    constructor(
+        stage,
+        tubeList,
+        // onCollision: () => void
+    ) {
+        stage.addChild(this.sprite);
+        this.sprite.anchor.set(0.5, 0.5);
+        this.updateTexture();
+        this.sprite.scale.x = 0.06;
+        this.sprite.scale.y = 0.06;
+        this.reset();
+
+        document.addEventListener("keydown", (e) => {
+            if (e.keyCode == 32) this.addSpeed(-GRAVITY / 3);
+        });
+        stage.on("pointerdown", () => this.addSpeed(-GRAVITY / 3));
+
+        setInterval(this.updateTexture, 200);
+    }
+}
+*/
+
+/*
+class Tube {
+    x = 0;
+    y = 0;
+    innerDistance = 80;
+    tubeWidth = 20;
+    sprite = new PIXI.Graphics();
+
+    reset(x = canvasWidthHeight + 20) {
+        this.x = x;
+        const tubeMinHeight = 60;
+        const randomNum =
+            Math.random() *
+            (canvasWidthHeight - 2 * tubeMinHeight - this.innerDistance);
+        this.y = tubeMinHeight + randomNum;
+    }
+
+    checkCollision(x, y, width, height) {
+        if (!(x + width < this.x || this.x + this.tubeWidth < x || this.y < y)) {
+            return true;
+        }
+        if (
+            !(
+                x + width < this.x ||
+                this.x + this.tubeWidth < x ||
+                y + height < this.y + this.innerDistance
+            )
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    update() {
+        this.x -= GAME_SPEED_X / 60;
+        if (this.x < -this.tubeWidth) this.reset();
+
+        this.sprite.clear();
+        this.sprite.beginFill(0xffffff, 1);
+        const { x, y, tubeWidth, innerDistance } = this;
+        this.sprite.drawRect(x, 0, tubeWidth, y);
+        this.sprite.drawRect(x, y + innerDistance, tubeWidth, canvasWidthHeight);
+        this.sprite.endFill();
+    }
+
+    constructor(stage, x) {
+        stage.addChild(this.sprite);
+        this.reset(x);
+    }
+}
+*/
+
+/*
+const renderer = PIXI.autoDetectRenderer(canvasWidthHeight, canvasWidthHeight, {
+    backgroundColor: 0xc1c2c4,
+});
+document.body.appendChild(renderer.view);
+const stage = new PIXI.Container();
+stage.interactive = true;
+stage.hitArea = new PIXI.Rectangle(0, 0, 1000, 1000);
+renderer.render(stage);
+
+const tubeList = TUBE_POS_LIST.map((d) => new Tube(stage, d));
+
+PIXI.loader.add(BIRD_FRAME_LIST).load(setup);
+
+let bird;
+const button = document.querySelector("#start");
+
+function setup() {
+    bird = new Bird(stage, tubeList, () => {
+        // Called when bird hit tube/ground/upper bound
+        gameFailed = true;
+        if (button) {
+            button.classList.remove("hide");
+        }
+    });
+    requestAnimationFrame(draw);
+}
+
+let gameStarted = false;
+let gameFailed = false;
+function draw() {
+    if (gameStarted) {
+        bird.updateSprite();
+        if (!gameFailed) tubeList.forEach((d) => d.update());
+    }
+    renderer.render(stage);
+    requestAnimationFrame(draw);
+}
+
+if (button) {
+    button.addEventListener("click", () => {
+        gameStarted = true;
+        button.innerHTML = "Retry";
+        if (gameFailed) {
+            gameFailed = false;
+            tubeList.forEach((d, i) => d.reset(TUBE_POS_LIST[i]));
+            bird.reset();
+        }
+        button.classList.add("hide");
+    });
+}
+*/
